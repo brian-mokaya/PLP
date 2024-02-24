9b67167aefa185101ed3995d8b1c9fbcee3d0ef4
brian-mokaya <132230013+brian-mokaya@users.noreply.github.com>
2024-02-24 20:39:13 +0300

    Python assignment one

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("enter your name : ")
+age = input ("enter your age : ")
+location = input ("enter tour location : ")
+print("hello"+name+"you are "+age+" years old and you live in "+location)
